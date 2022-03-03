628ee868693055e3169903a0c916a6917c700072
luisgs7 <luisgsilva270@gmail.com>
2022-03-03 11:18:01 -0300

    create models Categoria and Contato.

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,23 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
+    
