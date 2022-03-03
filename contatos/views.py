d7d0d6de2af3ec081f4d61136ea07adcab43eadb
luisgs7 <luisgsilva270@gmail.com>
2022-03-03 12:01:17 -0300

    add pagination and update error 404.

--- contatos/views.py
+++ contatos/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 3)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
