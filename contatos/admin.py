f8f7522865d1936afb0539f9f289d2b81569b59b
luisgs7 <luisgsilva270@gmail.com>
2022-03-03 11:17:44 -0300

    register models

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome')
+    list_display_links = ('sobrenome',)
+    list_filter = ('nome',)
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome')
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
+
