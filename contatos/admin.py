b823e8f0b2cafe9b9e3ee6506db9344cb0bb11a5
luisgs7 <luisgsilva270@gmail.com>
2022-03-03 12:00:25 -0300

    add list_display and list_editable

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome')
+    list_display = ('nome', 'sobrenome', 'mostrar', 'telefone')
     list_display_links = ('sobrenome',)
     list_filter = ('nome',)
     list_per_page = 10
     search_fields = ('nome', 'sobrenome')
+    list_editable = ('mostrar', 'telefone')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
