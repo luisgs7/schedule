77831ed00e61de36559e865b4d633b82c2a99341
luisgs7 <luisgsilva270@gmail.com>
2022-03-05 15:42:30 -0300

    config app accounts.

--- agenda/settings.py
+++ agenda/settings.py
@@ -44,8 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'contatos',
+    
+    # My Apps
+    'contatos.apps.ContatosConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
