81dbd568ef98215fbc66b330660beeca358c38b3
luisgs7 <luisgsilva270@gmail.com>
2022-03-05 10:34:04 -0300

    new migration, add imagefield

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.utils import timezone
 from django.db import models
 
@@ -18,6 +19,7 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/%d')
 
     def __str__(self):
         return self.nome
