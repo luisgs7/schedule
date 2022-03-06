759979990e234ad7e58b3a18a4a5e38dc9d8a6b1
luisgs7 <luisgsilva270@gmail.com>
2022-03-06 11:00:17 -0300

    New functions login, logout and login required for acess dashboard.

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,33 @@
 from curses import echo
+from email import message
 from django.shortcuts import redirect, render
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
 
+    usuario = request.POST.get('usuario')
+    password = request.POST.get('senha')
+
+    user = auth.authenticate(request, username=usuario, password=password)
+
+    if not user:
+        messages.error(request, 'Usuário ou senha inválidos.')
+        return render(request, 'accounts/login.html')
+
+    else:
+        auth.login(request, user)
+        messages.success(request, 'Login realizado com Sucesso.')
+        return redirect('dashboard')
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('login')
 
 
 def cadastro(request):
@@ -67,5 +84,6 @@ def cadastro(request):
     return redirect('login')
 
 
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
